df0d293f96ed1a3659f072137cd374c810ba4233
onion124567 <onion123>
2017-03-03 16:11:05 +0800

    add project
    添加项目

---
+++ app/src/main/java/com/hailaigo/factory/RequiresPresenter.java
@@ -0,0 +1,13 @@
+package com.hailaigo.factory;
+
+import com.hailaigo.presenter.Presenter;
+
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequiresPresenter {
+    Class<? extends Presenter> value();
+}
---
+++ app/src/main/java/com/hailaigo/factory/bean/UserInfoResponse.java
@@ -0,0 +1,9 @@
+package com.hailaigo.factory.bean;
+
+import java.util.List;
+
+/**
+ * Created by onion on 2016/7/27.
+ */
+public class UserInfoResponse {
+}
